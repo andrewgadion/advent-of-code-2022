1948047022ae03dee86d840cc819c0b63c8510be
Andrew Gadion <andrew.gadion@gmail.com>
2022-12-01 23:30:56 +0200

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun allCalories(input: List<String>) = buildList<Long> {
+        add(0)
+        input.forEach { if (it.isBlank()) add(0) else this[lastIndex] += it.toLong() }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        return allCalories(input).max()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: List<String>): Long {
+        return allCalories(input).sortedDescending().take(3).sum()
+    }
 
-    val input = readInput("Day01")
+    val input = readInput("day1")
     println(part1(input))
     println(part2(input))
 }
